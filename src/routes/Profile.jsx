ad37cabec5d441b9ebf3cb872d69a79e2fc31df2
carlos99at <carlos.abia99@gmail.com>
2024-04-09 19:05:53 +0200

    fix: import avatar

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
+import { Avatar } from '@nextui-org/react'
+import { useState } from 'react'
 
 export const Profile = () => {
-  const [file, setFile] = useState();
-  function handleChange(e) {
-      console.log(e.target.files);
-      setFile(URL.createObjectURL(e.target.files[0]));
+  const [file, setFile] = useState()
+  function handleChange (e) {
+    console.log(e.target.files)
+    setFile(URL.createObjectURL(e.target.files[0]))
   }
-  return <>
-  <Avatar src={file} className="w-20 h-20 text-large" />
-          <h2>Add Image:</h2>
-          <input type="file" onChange={handleChange} /></>
+  return (
+    <>
+      <Avatar src={file} className='w-20 h-20 text-large' />
+      <h2>Add Image:</h2>
+      <input type='file' onChange={handleChange} />
+    </>
+  )
 }
