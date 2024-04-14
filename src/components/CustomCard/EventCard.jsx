2901e839a94da154c9c2aa6e3c61ce99db0116f1
carlos99at <carlos.abia99@gmail.com>
2024-04-14 11:36:19 +0200

    style: event card name

--- src/components/CustomCard/EventCard.jsx
+++ src/components/CustomCard/EventCard.jsx
@@ -1,4 +1,4 @@
-import { Button, Card, Image } from '@nextui-org/react'
+import { Button, Card, Image, Link } from '@nextui-org/react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router'
 
@@ -7,41 +7,42 @@ export const EventCard = props => {
   const navigateTo = useNavigate()
   const user = useSelector(state => state.user)
   return (
-    <div
-      className='hover:cursor-pointer'
-      onClick={() => navigateTo(`/event/${id}`)}
-    >
-      <Card isHoverable className='rounded-lg shadow-md p-4'>
-        <div className='flex items-center h-full'>
-          <Image src={image} alt={name} width={100} />
-          <div className='flex-column justify-between pl-2 self-baseline w-full h-[6rem]'>
-            <div className='flex flex-responsive justify-between w-full'>
-              <p className='text-lg font-semibold'>{name}</p>
-              <p className='text-gray-500'>{type}</p>
-            </div>
-            <p className='hide-xs'>{description}</p>
-            {inExplore && (
-              <div className='self-end'>
-                {eventOwnerId === user.id ? (
-                  <p className='text-green-600'>Owner</p>
-                ) : (
-                  <Button
-                    className='z-20'
-                    onMouseMoveCapture={e => {
-                      e.stopPropagation()
-                      e.preventDefault()
-                    }}
-                    on
-                    color='primary'
-                  >
-                    Follow
-                  </Button>
-                )}
-              </div>
-            )}
+    <Card isHoverable className='rounded-lg shadow-md p-4'>
+      <div className='flex items-center h-full'>
+        <Image src={image} alt={name} width={100} />
+        <div className='flex-column justify-between pl-2 self-baseline w-full h-[6rem]'>
+          <div className='flex flex-responsive justify-between w-full'>
+            <Link
+              color='foreground'
+              onClick={() => navigateTo(`/event/${id}`)}
+              className='hover:cursor-pointer underline text-lg font-semibold'
+            >
+              {name}
+            </Link>
+            <p className='text-gray-500'>{type}</p>
           </div>
+          <p className='hide-xs'>{description}</p>
+          {inExplore && (
+            <div className='self-end'>
+              {eventOwnerId === user.id ? (
+                <p className='text-green-600'>Owner</p>
+              ) : (
+                <Button
+                  className='z-20'
+                  onMouseMoveCapture={e => {
+                    e.stopPropagation()
+                    e.preventDefault()
+                  }}
+                  on
+                  color='primary'
+                >
+                  Follow
+                </Button>
+              )}
+            </div>
+          )}
         </div>
-      </Card>
-    </div>
+      </div>
+    </Card>
   )
 }
