28043822f2192da9deab3ca47e1f5928beef33b2
carlos99at <carlos.abia99@gmail.com>
2024-05-07 19:17:34 +0200

    refactor: title test

--- cypress/e2e/create-event.cy.js
+++ cypress/e2e/create-event.cy.js
@@ -1,6 +1,6 @@
 import storageData from '../fixtures/storageData.json'
-describe('Create event', () => {
-  it('should create a new event', () => {
+describe('Create event and delete', () => {
+  it('should create a new event and delete it', () => {
     cy.visit('http://127.0.0.1:5173/', {
       onBeforeLoad: function (window) {
         window.localStorage.setItem('persist:root', JSON.stringify(storageData))
